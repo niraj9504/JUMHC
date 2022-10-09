16050a5b53d347bbcbac472cb75cd6a36c052bae
Niraj Kumar <niraj@NIRAJs-MacBook-Air.local>
2022-10-09 21:54:06 +0530

    JUMHC FRONTEND

---
+++ script.js
@@ -0,0 +1,14 @@
+const regName=document.getElementById("reg-name");
+const regEmail=document.getElementById("reg-email");
+const regPass=document.getElementById("reg-password");
+const regWp=document.getElementById("reg-wp");
+
+const logEmail=document.getElementById("log-email");
+const logPass=document.getElementById("log-pass");
+
+function regSubmit(){
+   alert("you have successfully registered!")
+}
+function logSubmit(){
+    alert("you have successfully logged in!")
+ }
